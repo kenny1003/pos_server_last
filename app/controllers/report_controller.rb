2c7c1de39716103212faa88e4be0808dc79388e9
Tongil Kim <xhddlf8070@gmail.com>
2017-04-06 00:32:35 +0000

    report

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,7 +1,8 @@
 class ReportController < ApplicationController
 
   def index
-    
+    require "rubygems"
+    require "will_paginate"
     array_order=["id","id desc","created_at","created_at desc","totalprice","totalprice desc"]
     if params[:value].blank?
       @order_count=0
@@ -12,15 +13,9 @@ class ReportController < ApplicationController
     puts @order_count
     puts array_order[@order_count]
     order_way=array_order[@order_count]
-    @bill_all = current_user.store.bills.order("#{order_way}")
-    
+    #@bill_all = current_user.store.bills.order("#{order_way}")
+    @bill_all=current_user.store.bills.paginate(:page => params[:page], :per_page => 10).order("#{order_way}")
 
        # @bill_all=@bill_all.paginate(:page => params[:page], :per_page => 10)
-
-
-
-
-
-
   end
 end
